0fcbfaedbdeacb9e2fb56e08c960c0568eafb774
khkr <32921476+khkred@users.noreply.github.com>
2022-04-19 13:36:24 +0530

    boolean and loops

---
+++ boolean.dart
@@ -0,0 +1,19 @@
+
+
+void main() {
+
+//true, false
+
+var a = true;
+
+print("Value of a is $a");
+var b = false;
+
+var num1 = 25;
+var num2 = 33;
+
+a = num1>num2;
+print("Is $num1 > $num2: $a ");
+
+}
+
