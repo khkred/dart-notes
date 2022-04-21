5bafbbc323fa195d856898318b1944b9133dbe85
khkr <32921476+khkred@users.noreply.github.com>
2022-04-21 11:51:03 +0530

    Added lists basic code

---
+++ lists.dart
@@ -0,0 +1,16 @@
+void main() 
+{
+    // List
+    var b = [1,2,3,4];
+
+    var studentNames = ["Arjun","Ramya","Robert","Anthony"];
+
+    print(studentNames[2]);
+
+    print(studentNames.size());
+
+
+
+
+    
+}
